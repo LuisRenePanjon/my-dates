724b467b7780886909ddd43b5ea26bbe027aa016
Luis René Panjón <luchorene97@gmail.com>
2022-12-28 16:09:25 -0500

    Crear componente inicial de aplicación

---
+++ src/MyDatesApp.jsx
@@ -0,0 +1,7 @@
+export const MyDatesApp = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>My Dates</h1>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { MyDatesApp } from './MyDatesApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MyDatesApp />
+  </React.StrictMode>
 )
