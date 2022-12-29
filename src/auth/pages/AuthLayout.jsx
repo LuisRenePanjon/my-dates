1799edd985732a2a53cfbde7239f88729f124590
Luis René Panjón <luchorene97@gmail.com>
2022-12-29 12:08:41 -0500

    fix: Path de imágenes

--- src/auth/pages/AuthLayout.jsx
+++ src/auth/pages/AuthLayout.jsx
@@ -9,7 +9,7 @@ export const AuthLayout = ({ children }) => {
       <div className='lg:mx-[40rem]'>
         <div className='sm:flex sm:row  items-center sm:justify-between'>
           <h1 className='text-2xl text-slate-200 pl-4 pr-20 max-sm:my-6'>Tu tiempo es valioso, administralo correctamente. <br /> Tu diversión, trabajo y familia perfectamente equilibrado.</h1>
-          <img src='/src/assets/profesional.jpg' alt='Imágen animada de personas con un calendario' className=' rounded-xl mx-3 sm:w-[600px] max-sm:w-56' />
+          <img src={'/assets/profesional.jpg'} alt='Imágen animada de personas con un calendario' className=' rounded-xl mx-3 sm:w-[600px] max-sm:w-56' />
         </div>
 
         <div className='flex items-center justify-center'>
